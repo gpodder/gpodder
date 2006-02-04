811e376eb6174768e555f4580c42bb9eb1f2269f
Thomas Perl <thp@perli.net>
2006-02-04 13:21:07 +0000

    uhh.. forgot to add the new files to the repository.. :)
    git-svn-id: svn://svn.berlios.de/gpodder@29 b0d088ad-0a06-0410-aad2-9ed5178a7e87

---
+++ src/gpodder/liblocdbreader.py
@@ -0,0 +1,90 @@
+
+#
+# gPodder
+# Copyright (c) 2005-2006 Thomas Perl <thp@perli.net>
+# Released under the GNU General Public License (GPL)
+#
+
+#
+#  liblocdbreader.py -- xml reader functionality
+#  thomas perl <thp@perli.net>   20060110
+#
+#
+
+from xml.sax.saxutils import DefaultHandler
+from xml.sax.handler import ErrorHandler
+from xml.sax import make_parser
+from string import strip
+
+import libpodcasts
+import libgpodder
+
+class rssLocDBErrorHandler( ErrorHandler):
+    def __init__( self):
+        None
+
+    def error( self, exception):
+        print exception
+
+    def fatalError( self, exception):
+        print "FATAL ERROR: ", exception
+
+    def warning( self, exception):
+        print "warning: ", exception
+
+class readLocalDB( DefaultHandler):
+    channel_url = ""
+    channel = None
+    current_item = None
+    current_element_data = ""
+
+    def __init__( self):
+        None
+    
+    def parseXML( self, url, filename):
+        self.channel_url = url
+        parser = make_parser()
+	parser.returns_unicode = True
+        parser.setContentHandler( self)
+	parser.setErrorHandler( rssLocDBErrorHandler())
+        parser.parse( filename)
+    
+    def startElement( self, name, attrs):
+        self.current_element_data = ""
+        if libgpodder.isDebugging():
+            print "(readLocalDB) startElement: " + name
+
+        if name == "channel":
+            self.channel = libpodcasts.podcastChannel( self.channel_url)
+        if name == "item":
+            self.current_item = libpodcasts.podcastItem()
+    
+    def endElement( self, name):
+        if libgpodder.isDebugging():
+            print "(readLocalDB) endElement: " + name
+        
+        if self.current_item == None:
+            if name == "title":
+                self.channel.title = self.current_element_data
+            if name == "link":
+                self.channel.link = self.current_element_data
+            if name == "description":
+                self.channel.description = self.current_element_data
+        
+        if self.current_item != None:
+            if name == "title":
+                self.current_item.title = self.current_element_data
+            if name == "url":
+                self.current_item.url = self.current_element_data
+            if name == "description":
+                self.current_item.description = self.current_element_data
+            if name == "item":
+                self.channel.addItem( self.current_item)
+                if libgpodder.isDebugging():
+                    print "importing local db channel: " + self.current_item.url
+                self.current_item = None
+    
+    def characters( self, ch):
+        self.current_element_data = self.current_element_data + ch
+
+
