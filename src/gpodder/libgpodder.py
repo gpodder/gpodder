9c4d39a885741ffe61f349598364778dbf72b154
Thomas Perl <thp@perli.net>
2006-07-30 13:17:27 +0000

    we're in development again! :) pushed version number added new makefile target: "make cl" (changelog update) set debugging variable to true again
    git-svn-id: svn://svn.berlios.de/gpodder/trunk@146 b0d088ad-0a06-0410-aad2-9ed5178a7e87

--- src/gpodder/libgpodder.py
+++ src/gpodder/libgpodder.py
@@ -61,7 +61,7 @@ from xml.sax import saxutils
 
 # global debugging variable, set to False on release
 # TODO: while developing a new version, set this to "True"
-debugging = False
+debugging = True
 
 # global recursive lock for thread exclusion
 globalLock = threading.RLock()
