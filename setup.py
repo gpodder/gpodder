02860b011498bf6fa4015938306de03840305391
Thomas Perl <thp@perli.net>
2007-03-07 00:00:08 +0000

    package cleanup changes
    git-svn-id: svn://svn.berlios.de/gpodder/trunk@257 b0d088ad-0a06-0410-aad2-9ed5178a7e87

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ for l in languages:
 
 # files to install
 inst_manpages = glob.glob( 'doc/man/*.1')
-inst_images   = glob.glob('data/artwork/*')
+inst_images   = glob.glob('data/artwork/*.{png,gif}')
 inst_icons    = [ 'data/gpodder.png' ] + glob.glob('data/gpodder-??x??.png')
 inst_share    = [ 'data/gpodder.glade', 'data/gpodder-48x48.png' ]
 inst_desktop  = [ 'data/gpodder.desktop' ]
