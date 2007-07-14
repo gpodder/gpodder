eb959deaf58ef688d8817a77de7459d61e1f524f
Thomas Perl <thp@perli.net>
2007-07-14 10:50:18 +0000

    add ukrainian translation by Mykola Nikishov
    git-svn-id: svn://svn.berlios.de/gpodder/trunk@352 b0d088ad-0a06-0410-aad2-9ed5178a7e87

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from distutils.core import setup
 gpodder_version = os.popen( "cat bin/gpodder |grep ^__version__.*=|cut -d\\\" -f2").read().strip()
 
 # translations
-languages = [ "de", "fr", "sv", "it", "pt", "es", "nl", "ru" ]
+languages = [ "de", "fr", "sv", "it", "pt", "es", "nl", "ru", "uk" ]
 translation_files = []
 
 # add translated files to translations dictionary
