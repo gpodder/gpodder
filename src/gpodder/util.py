d2207f2c7168e7547aa63be95ff762635d1e0f27
Thomas Perl <thp@perli.net>
2007-12-01 14:20:57 +0000

    Fix a fatal bug with first-run that prevents channel list saving
    git-svn-id: svn://svn.berlios.de/gpodder/trunk@476 b0d088ad-0a06-0410-aad2-9ed5178a7e87

--- src/gpodder/util.py
+++ src/gpodder/util.py
@@ -142,7 +142,15 @@ def get_free_disk_space(path):
     """
     Calculates the free disk space available to the current user
     on the file system that contains the given path.
+
+    If the path (or its parent folder) does not yet exist, this
+    function returns zero.
     """
+
+    path = os.path.dirname(path)
+    if not os.path.exists(path):
+        return 0
+
     s = os.statvfs(path)
 
     return s.f_bavail * s.f_bsize
