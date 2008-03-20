35a75d7c60b620094b7a9ab3724127be672b9ab9
Thomas Perl <thp@perli.net>
2008-03-20 10:10:35 +0000

    Thu, 20 Mar 2008 11:09:27 +0100 <thp@perli.net> Timestamp and benchmark functions for logger
    	* src/gpodder/liblogger.py: Add support for timestamping and benchmark
    	functions to liblogger, so we see the microtime since gPodder startup
    	and can also use the logging module to benchmark the speed of certain
    	code parts; this is helpful for our performance patches :)
    
    
    
    git-svn-id: svn://svn.berlios.de/gpodder/trunk@619 b0d088ad-0a06-0410-aad2-9ed5178a7e87

--- src/gpodder/liblogger.py
+++ src/gpodder/liblogger.py
@@ -24,6 +24,7 @@
 #
 
 import traceback
+import time
 
 write_to_stdout = False
 
@@ -32,12 +33,20 @@ def enable_verbose():
     global write_to_stdout
     write_to_stdout = True
 
+first_time = time.time()
+last_times = []
 
 def log( message, *args, **kwargs):
+    global first_time
+    global last_times
     if 'sender' in kwargs:
         message = '(%s) %s' % ( kwargs['sender'].__class__.__name__, message )
+    if 'bench_start' in kwargs:
+        last_times.append(time.time())
+    if 'bench_end' in kwargs and len(last_times) > 0:
+        message += (' (benchmark: %.4f seconds)' % (time.time()-(last_times.pop())))
     if write_to_stdout:
-        print message % args
+        print (('[%8.3f] ' % (time.time()-first_time)) + message) % args
         if kwargs.get( 'traceback', False):
             error = traceback.format_exc()
             if error.strip() != 'None':
