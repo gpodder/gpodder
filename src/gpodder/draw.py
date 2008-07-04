21096f472f3c7d391539ed65c73f6d23079d5804
Thomas Perl <thp@perli.net>
2008-07-04 16:47:51 +0000

    Fri, 04 Jul 2008 18:47:19 +0200 <thp@perli.net> New look for the "pill" pixbuf
    	* src/gpodder/draw.py: Refresh the look of the "pill" pixbuf code, so
    	we have a more visually pleasing status display in the podcast list
    
    
    
    git-svn-id: svn://svn.berlios.de/gpodder/trunk@755 b0d088ad-0a06-0410-aad2-9ed5178a7e87

--- src/gpodder/draw.py
+++ src/gpodder/draw.py
@@ -39,28 +39,35 @@ class TextExtents(object):
 RRECT_LEFT_SIDE = 1
 RRECT_RIGHT_SIDE = 2
 
-def draw_rounded_rectangle(ctx, x, y, w, h, r=10, left_side_width = None, sides_to_draw=0):
+def draw_rounded_rectangle(ctx, x, y, w, h, r=10, left_side_width = None, sides_to_draw=0, close=False):
     if left_side_width is None:
-        left_side_width = w/2
+        left_side_width = flw/2
+    
+    x = int(x)
+    offset = .5 if close else 0
 
     if sides_to_draw & RRECT_LEFT_SIDE:
-        ctx.move_to(x+left_side_width, y+h)
+        ctx.move_to(x+int(left_side_width)-offset, y+h)
         ctx.line_to(x+r, y+h)
         ctx.curve_to(x, y+h, x, y+h, x, y+h-r)
         ctx.line_to(x, y+r)
         ctx.curve_to(x, y, x, y, x+r, y)
-        ctx.line_to(x+left_side_width, y)
+        ctx.line_to(x+int(left_side_width)-offset, y)
+        if close:
+            ctx.line_to(x+int(left_side_width)-offset, y+h)
 
     if sides_to_draw & RRECT_RIGHT_SIDE:
-        ctx.move_to(x+left_side_width, y)
+        ctx.move_to(x+int(left_side_width)+offset, y)
         ctx.line_to(x+w-r, y)
         ctx.curve_to(x+w, y, x+w, y, x+w, y+r)
         ctx.line_to(x+w, y+h-r)
         ctx.curve_to(x+w, y+h, x+w, y+h, x+w-r, y+h)
-        ctx.line_to(x+left_side_width, y+h)
+        ctx.line_to(x+int(left_side_width)+offset, y+h)
+        if close:
+            ctx.line_to(x+int(left_side_width)+offset, y)
 
 
-def draw_text_pill(left_text, right_text, x=0, y=0, border=3, radius=11):
+def draw_text_pill(left_text, right_text, x=0, y=0, border=4, radius=14):
     # Create temporary context to calculate the text size
     ctx = cairo.Context(cairo.ImageSurface(cairo.FORMAT_ARGB32, 1, 1))
 
@@ -68,8 +75,10 @@ def draw_text_pill(left_text, right_text, x=0, y=0, border=3, radius=11):
     widget = gtk.ProgressBar()
     style = widget.rc_get_style()
 
+    x_border = int(border*1.2)
+
     font_desc = style.font_desc
-    font_size = float(font_desc.get_size())/float(pango.SCALE)
+    font_size = float(1.15*font_desc.get_size())/float(pango.SCALE)
     font_name = font_desc.get_family()
 
     ctx.set_font_size(font_size)
@@ -78,9 +87,9 @@ def draw_text_pill(left_text, right_text, x=0, y=0, border=3, radius=11):
     left_text_e = TextExtents(ctx, left_text)
     right_text_e = TextExtents(ctx, right_text)
     text_height = max(left_text_e.height, right_text_e.height)
-    
+
     image_height = int(y+text_height+border*2)
-    image_width = int(x+left_text_e.width+right_text_e.width+border*4)
+    image_width = int(x+left_text_e.width+right_text_e.width+x_border*4)
     surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, image_width, image_height)
 
     ctx = cairo.Context(surface)
@@ -92,32 +101,44 @@ def draw_text_pill(left_text, right_text, x=0, y=0, border=3, radius=11):
     if right_text == '0':
         right_text = None
 
-    left_side_width = left_text_e.width + border*2
-    right_side_width = right_text_e.width + border*2
+    left_side_width = left_text_e.width + x_border*2
+    right_side_width = right_text_e.width + x_border*2
 
     rect_width = left_side_width + right_side_width
     rect_height = text_height + border*2
     if left_text is not None:
-        draw_rounded_rectangle(ctx,x,y,rect_width,rect_height,radius, left_side_width, RRECT_LEFT_SIDE)
-        ctx.set_source_rgba( 0, 0, 0, 0.5)
-        ctx.fill()
+        draw_rounded_rectangle(ctx,x,y,rect_width,rect_height,radius, left_side_width, RRECT_LEFT_SIDE, right_text is None)
+        linear = cairo.LinearGradient(x, y, x+left_side_width/2, y+rect_height/2)
+        linear.add_color_stop_rgba(0, .7, .7, .7, .5)
+        linear.add_color_stop_rgba(1, .4, .4, .4, .5)
+        ctx.set_source(linear)
+        ctx.fill_preserve()
+        ctx.set_source_rgba(0, 0, 0, .4)
+        ctx.set_line_width(1)
+        ctx.stroke()
 
-        ctx.move_to(x+1+border-left_text_e.x_bearing, y+1+border+text_height)
+        ctx.move_to(x+1+x_border-left_text_e.x_bearing, y+1+border+text_height)
         ctx.set_source_rgba( 0, 0, 0, 1)
         ctx.show_text(left_text)
-        ctx.move_to(x+border-left_text_e.x_bearing, y+border+text_height)
+        ctx.move_to(x+x_border-left_text_e.x_bearing, y+border+text_height)
         ctx.set_source_rgba( 1, 1, 1, 1)
         ctx.show_text(left_text)
 
     if right_text is not None:
-        draw_rounded_rectangle(ctx, x, y, rect_width, rect_height, radius, left_side_width, RRECT_RIGHT_SIDE)
-        ctx.set_source_rgba( 0, 0, 0, 0.7)
-        ctx.fill()
+        draw_rounded_rectangle(ctx, x, y, rect_width, rect_height, radius, left_side_width, RRECT_RIGHT_SIDE, left_text is None)
+        linear = cairo.LinearGradient(x+left_side_width, y, x+left_side_width+right_side_width/2, y+rect_height)
+        linear.add_color_stop_rgba(0, 0, 0, 0, .9)
+        linear.add_color_stop_rgba(1, 0, 0, 0, .5)
+        ctx.set_source(linear)
+        ctx.fill_preserve()
+        ctx.set_source_rgba(0, 0, 0, .7)
+        ctx.set_line_width(1)
+        ctx.stroke()
 
-        ctx.move_to(x+1+border*3+left_text_e.width-right_text_e.x_bearing, y+1+border+text_height)
+        ctx.move_to(x+1+x_border*3+left_text_e.width-right_text_e.x_bearing, y+1+border+text_height)
         ctx.set_source_rgba( 0, 0, 0, 1)
         ctx.show_text(right_text)
-        ctx.move_to(x+border*3+left_text_e.width-right_text_e.x_bearing, y+border+text_height)
+        ctx.move_to(x+x_border*3+left_text_e.width-right_text_e.x_bearing, y+border+text_height)
         ctx.set_source_rgba( 1, 1, 1, 1)
         ctx.show_text(right_text)
 
