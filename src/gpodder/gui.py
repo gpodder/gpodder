31ab62ec33c91ad8591dd0be726c6f65fef376cd
Steven Oxley <steven.aj.oxley@gmail.com>
2009-03-22 19:22:59 +0530

    Added the ability to resize episode columns
    * Added ability to resize episode columns in the main episode list
    * Added ability to resize episode columns in the downloads list

--- src/gpodder/gui.py
+++ src/gpodder/gui.py
@@ -587,6 +587,7 @@ class gPodder(GladeWidget, dbus.service.Object):
         namecell.set_property('ellipsize', pango.ELLIPSIZE_END)
         namecolumn = gtk.TreeViewColumn(_("Episode"), namecell, markup=6)
         namecolumn.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+        namecolumn.set_resizable(True)
         namecolumn.set_expand(True)
 
         sizecell = gtk.CellRendererText()
@@ -622,6 +623,7 @@ class gPodder(GladeWidget, dbus.service.Object):
         episodecell.set_property('ellipsize', pango.ELLIPSIZE_END)
         episodecolumn = gtk.TreeViewColumn( _("Episode"), episodecell, text=0)
         episodecolumn.set_sizing(gtk.TREE_VIEW_COLUMN_AUTOSIZE)
+        episodecolumn.set_resizable(True)
         episodecolumn.set_expand(True)
         
         speedcell = gtk.CellRendererText()
