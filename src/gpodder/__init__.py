927d3d9646a541e92dc798b719e6065d2f8f20d3
Thomas Perl <thp@thpinfo.com>
2010-07-07 19:28:33 +0200

    gPodder 2.7 "Proposition Infinity" released
    Updated README, manpages, version and release date.

--- src/gpodder/__init__.py
+++ src/gpodder/__init__.py
@@ -18,8 +18,8 @@
 #
 
 __author__    = 'Thomas Perl <thp@gpodder.org>'
-__version__   = '2.6'
-__date__      = '2010-05-23'
+__version__   = '2.7'
+__date__      = '2010-07-07'
 __copyright__ = '© 2005-2010 Thomas Perl and the gPodder Team'
 __licence__   = 'GNU General Public License, version 3 or later'
 __url__       = 'http://gpodder.org/'
