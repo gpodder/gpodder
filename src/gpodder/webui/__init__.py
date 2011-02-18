d230bfb8f27a56b4d6bd95f5285b234890ce9d9d
Thomas Perl <m@thp.io>
2011-02-18 20:47:11 +0100

    Web UI: Very crappy testing code ;)

--- src/gpodder/webui/__init__.py
+++ src/gpodder/webui/__init__.py
@@ -28,9 +28,12 @@ from gpodder import model
 
 import BaseHTTPServer
 
+import os
+import re
 
 class WebUI(BaseHTTPServer.BaseHTTPRequestHandler):
     core = None
+    player = None
 
     @classmethod
     def run(cls, server_class=BaseHTTPServer.HTTPServer):
@@ -40,18 +43,118 @@ class WebUI(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
-        self.end_headers()
-        if self.path == '/podcasts':
-            print >>self.wfile, '<h1>Podcasts</h1>'
-            for podcast in model.Model.get_podcasts(self.core.db):
-                print >>self.wfile, '<li>', podcast.title, '<code>', \
-                        podcast.get_statistics(), '</code></li>'
+        if re.match('/coverart/\d+', self.path):
+            self.send_header('Content-type', 'image/jpeg')
+        elif self.path == '/logo':
+            self.send_header('Content-type', 'image/png')
         else:
-            self.wfile.write('<a href="/podcasts">Podcasts</a>')
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        if re.match('/coverart/\d+', self.path):
+            id = int(self.path[10:])
+            for podcast in model.Model.get_podcasts(self.core.db):
+                if podcast.id == id:
+                    self.wfile.write(open(podcast.cover_file).read())
+                    break
+            return
+        elif self.path == '/logo':
+            fn = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', 'gpodder.png')
+            self.wfile.write(open(fn).read())
+            return
+
+        print >>self.wfile, """
+        <html><head><title>gPodder WebUI</title>
+        <style type="text/css">
+          body {
+              font-family: sans-serif;
+              background-image: url(/logo);
+              background-repeat: no-repeat;
+          }
+          h1 {
+              margin: 0px;
+              padding: 0px;
+              padding-bottom: 30px;
+              padding-left: 60px;
+              font-size: 12pt;
+          }
+          ul {
+              margin: 0px;
+              padding: 0px;
+              list-style: none;
+          }
+          li {
+              padding-top: 1px;
+          }
+          li a {
+              display: block;
+              padding: 7px;
+              background-color: #ccc;
+          }
+          a {
+              color: black;
+              text-decoration: none;
+          }
+          img {
+              border: 0px;
+              width: 30px;
+              height: 30px;
+              vertical-align: middle;
+          }
+        </style>
+        </head>
+        <body>
+        """
+        if self.path == '/podcast':
+            print >>self.wfile, '<h1>Podcasts</h1><ul>'
+            for podcast in model.Model.get_podcasts(self.core.db):
+                print >>self.wfile, \
+                        '<li><a href="/podcast/%d"><img src="/coverart/%d"> %s</a></li>' % \
+                        (podcast.id, podcast.id, podcast.title + ' DLs:' + str(podcast.get_statistics()[3]))
+        elif re.match('/podcast/\d+$', self.path):
+            id = int(self.path[9:])
+            for podcast in model.Model.get_podcasts(self.core.db):
+                if podcast.id != id:
+                    continue
+
+                print >>self.wfile, '<h1><a href="/podcast">back</a> | %s</h1><ul>' % \
+                        podcast.title
+
+                for episode in podcast.get_all_episodes():
+                    print >>self.wfile, '<li>'
+                    if episode.was_downloaded(and_exists=True):
+                        print >>self.wfile, '<strong>'
+                    print >>self.wfile, '<a href="/podcast/%d/%d">%s</a>' % \
+                            (podcast.id, episode.id, episode.title)
+                    if episode.was_downloaded(and_exists=True):
+                        print >>self.wfile, '</strong>'
+                    print >>self.wfile, '</li>'
+        elif re.match('/podcast/\d+/\d+', self.path):
+            podcast_id, id= [int(x) for x in self.path[9:].split('/')]
+
+            for podcast in model.Model.get_podcasts(self.core.db):
+                if podcast.id != podcast_id:
+                    continue
+
+                for episode in podcast.get_all_episodes():
+                    if episode.id == id:
+                        print >>self.wfile, '<h1><a href="/podcast/%d">back</a> | %s</h1><ul>' % \
+                                (podcast.id, episode.title)
+                        print 'playing:', episode.local_filename(create=False)
+                        if self.player is not None:
+                            self.player.mediaPlay(episode.local_filename(create=False))
+                            print >>self.wfile, repr(self.player.mediaPlayInfo()).replace('<', '&lt;')
+                        print >>self.wfile, '<p>',episode.description,'</p>'
+        else:
+            self.wfile.write('<a href="/podcast">Podcasts</a>')
 
 
 def main():
     WebUI.core = core.Core()
+    try:
+        import android
+        WebUI.player = android.Android()
+    except:
+        pass
     return WebUI.run()
 
