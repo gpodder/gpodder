4d9ad45a8c6e53c2115c230e5feccb96055b297b
Thomas Perl <m@thp.io>
2011-04-06 13:06:50 +0200

    Remove gstreamer track length detection (bug 1316)
    This is just too crashy :/

--- src/gpodder/model.py
+++ src/gpodder/model.py
@@ -27,7 +27,6 @@ import gpodder
 from gpodder import util
 from gpodder import feedcore
 from gpodder import youtube
-from gpodder import gstreamer
 
 from gpodder.liblogger import log
 
@@ -856,21 +855,6 @@ class PodcastEpisode(PodcastModelObject):
         self.state = gpodder.STATE_DOWNLOADED
         self.is_played = False
         self.length = os.path.getsize(filename)
-
-        if not self.total_time:
-            try:
-                length = gstreamer.get_track_length(filename)
-                if length is not None:
-                    length = int(length/1000)
-                    log('Detected media length: %d seconds', length, \
-                            sender=self)
-                    self.total_time = length
-                    self.db.save_episode(self)
-                    self.db.commit()
-                    return
-            except Exception, e:
-                log('Error while detecting media length: %s', str(e), \
-                        sender=self)
 
         self.db.save_downloaded_episode(self)
         self.db.commit()
