34b54e94b8387848d6549611a2ab9670d5307d8b
Thomas Perl <m@thp.io>
2011-07-15 17:57:13 +0200

    Remove "progressbar_pixbuf" from gpodder.gtkui.draw

--- src/gpodder/gtkui/draw.py
+++ src/gpodder/gtkui/draw.py
@@ -250,38 +250,3 @@ def cairo_surface_to_pixbuf(s):
     pixbuf = pbl.get_pixbuf()
     return pixbuf
 
-def progressbar_pixbuf(width, height, percentage):
-    COLOR_BG = (.4, .4, .4, .4)
-    COLOR_FG = (.2, .9, .2, 1.)
-    COLOR_FG_HIGH = (1., 1., 1., .5)
-    COLOR_BORDER = (0., 0., 0., 1.)
-
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
-    ctx = cairo.Context(surface)
-
-    padding = int(float(width)/8.0)
-    bar_width = 2*padding
-    bar_height = height - 2*padding
-    bar_height_fill = bar_height*percentage
-
-    # Background
-    ctx.rectangle(padding, padding, bar_width, bar_height)
-    ctx.set_source_rgba(*COLOR_BG)
-    ctx.fill()
-
-    # Foreground
-    ctx.rectangle(padding, padding+bar_height-bar_height_fill, bar_width, bar_height_fill)
-    ctx.set_source_rgba(*COLOR_FG)
-    ctx.fill()
-    ctx.rectangle(padding+bar_width/3, padding+bar_height-bar_height_fill, bar_width/4, bar_height_fill)
-    ctx.set_source_rgba(*COLOR_FG_HIGH)
-    ctx.fill()
-
-    # Border
-    ctx.rectangle(padding-.5, padding-.5, bar_width+1, bar_height+1)
-    ctx.set_source_rgba(*COLOR_BORDER)
-    ctx.set_line_width(1.)
-    ctx.stroke()
-
-    return cairo_surface_to_pixbuf(surface)
-
