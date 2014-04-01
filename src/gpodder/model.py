c576997c4ae9f32639d1bf8acccff18e8175c031
Thomas Perl <m@thp.io>
2014-04-01 22:35:39 +0200

    model: Add rewrite_url() function. Bug 1905

--- src/gpodder/model.py
+++ src/gpodder/model.py
@@ -807,6 +807,17 @@ class PodcastChannel(PodcastModelObject):
         else:
             logger.warn('Cannot set strategy to %d', download_strategy)
 
+    def rewrite_url(self, new_url):
+        new_url = util.normalize_feed_url(new_url)
+        if new_url is None:
+            return None
+
+        self.url = new_url
+        self.http_etag = None
+        self.http_last_modified = None
+        self.save()
+        return new_url
+
     def check_download_folder(self):
         """Check the download folder for externally-downloaded files
 
