b0d83b90bfb09083c4c7f2b03fb22c18705b74f9
Thomas Perl <m@thp.io>
2014-05-17 12:01:34 +0200

    gPodder 3.7.0 "Off-Screen Bionic Chiseling" released

--- src/gpodder/__init__.py
+++ src/gpodder/__init__.py
@@ -20,9 +20,9 @@
 # This metadata block gets parsed by setup.py - use single quotes only
 __tagline__   = 'Media aggregator and podcast client'
 __author__    = 'Thomas Perl <thp@gpodder.org>'
-__version__   = '3.6.1'
-__date__      = '2014-03-08'
-__relname__   = 'Little Orphan Airplane'
+__version__   = '3.7.0'
+__date__      = '2014-05-17'
+__relname__   = 'Off-Screen Bionic Chiseling'
 __copyright__ = '© 2005-2014 Thomas Perl and the gPodder Team'
 __license__   = 'GNU General Public License, version 3 or later'
 __url__       = 'http://gpodder.org/'
