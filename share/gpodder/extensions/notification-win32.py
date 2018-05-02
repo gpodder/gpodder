0f88112905f3d1e26ce8f3f5964445375d770ed8
Eric Le Lay <elelay@macports.org>
2018-05-02 10:59:03 +0200

    notifications as Gtk.MessageDialog on windows

--- share/gpodder/extensions/notification-win32.py
+++ share/gpodder/extensions/notification-win32.py
@@ -19,7 +19,23 @@
 
 # Notification implementation for Windows
 # Sean Munkel; 2012-12-29
-
+"""
+Notification implementation for Windows.
+Current state (2018/05/01 ELL):
+ - I can't get pywin32 to work in msys2 (the platform used for this python3/gtk3 installer)
+   so existing code using COM doesn't work.
+ - Gio.Notification is not implemented on windows yet.
+   see https://bugzilla.gnome.org/show_bug.cgi?id=776583
+ - Gtk.StatusIcon with a context works but is deprecated. Showing a balloon using set_tooltip_markup
+   doesn't work.
+   See https://github.com/afiskon/py-gtk-example
+ - in-app notifications using an overlay are pleasing to the eye, but don't solve the problem
+   see https://stackoverflow.com/questions/45431512/gtk-in-app-notifications-api-referece
+ - hexchat have implemented a solid c++ solution.
+   See https://github.com/hexchat/hexchat/tree/master/src/fe-gtk/notifications
+I've chosen to implement a cheap workaround using gtk.MessageDialog because
+Gio.Notification will likely be implemented in 6 months-1 year
+"""
 __title__ = 'Notification Bubbles for Windows'
 __description__ = 'Display notification bubbles for different events.'
 __authors__ = 'Sean Munkel <SeanMunkel@gmail.com>'
@@ -30,90 +46,32 @@ __only_for__ = 'win32'
 import functools
 import os
 import os.path
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
 import gpodder
-import pywintypes
-import win32gui
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-IDI_APPLICATION = 32512
-WM_TASKBARCREATED = win32gui.RegisterWindowMessage('TaskbarCreated')
-WM_TRAYMESSAGE = 1044
-
-
-# based on http://code.activestate.com/recipes/334779/
-class NotifyIcon(object):
-    def __init__(self, hwnd):
-        self._hwnd = hwnd
-        self._id = 0
-        self._flags = win32gui.NIF_MESSAGE | win32gui.NIF_ICON
-        self._callbackmessage = WM_TRAYMESSAGE
-        path = os.path.join(os.path.dirname(__file__), '..', '..',
-                'icons', 'hicolor', '16x16', 'apps', 'gpodder.ico')
-        icon_path = os.path.abspath(path)
-
-        try:
-            self._hicon = win32gui.LoadImage(None, icon_path, 1, 0, 0, 0x50)
-        except pywintypes.error as e:
-            logger.warn("Couldn't load gpodder icon for tray")
-            self._hicon = win32gui.LoadIcon(0, IDI_APPLICATION)
-
-        self._tip = ''
-        self._info = ''
-        self._timeout = 0
-        self._infotitle = ''
-        self._infoflags = win32gui.NIIF_NONE
-        win32gui.Shell_NotifyIcon(win32gui.NIM_ADD, self.notify_config_data)
-
-    @property
-    def notify_config_data(self):
-        """ Function to retrieve the NOTIFYICONDATA Structure. """
-        return (self._hwnd, self._id, self._flags, self._callbackmessage,
-                self._hicon, self._tip, self._info, self._timeout,
-                self._infotitle, self._infoflags)
-
-    def remove(self):
-        """ Removes the tray icon. """
-        win32gui.Shell_NotifyIcon(win32gui.NIM_DELETE,
-                self.notify_config_data)
-
-    def set_tooltip(self, tooltip):
-        """ Sets the tray icon tooltip. """
-        self._flags = self._flags | win32gui.NIF_TIP
-        self._tip = tooltip.encode("mbcs")
-        win32gui.Shell_NotifyIcon(win32gui.NIM_MODIFY,
-                self.notify_config_data)
-
-    def show_balloon(self, title, text, timeout=10,
-            icon=win32gui.NIIF_NONE):
-        """ Shows a balloon tooltip from the tray icon. """
-        self._flags = self._flags | win32gui.NIF_INFO
-        self._infotitle = title.encode("mbcs")
-        self._info = text.encode("mbcs")
-        self._timeout = timeout * 1000
-        self._infoflags = icon
-        win32gui.Shell_NotifyIcon(win32gui.NIM_MODIFY,
-                self.notify_config_data)
-
 
 class gPodderExtension(object):
     def __init__(self, *args):
-        self.notifier = None
+        self._window = None
 
     def on_ui_object_available(self, name, ui_object):
-        def callback(self, window, *args):
-            self.notifier = NotifyIcon(window.window.handle)
-
         if name == 'gpodder-gtk':
-            ui_object.main_window.connect('realize',
-                    functools.partial(callback, self))
+            self._window = ui_object.main_window
 
     def on_notification_show(self, title, message):
-        if self.notifier is not None:
-            self.notifier.show_balloon(title, message)
+        if self._window:
+            dialog = Gtk.MessageDialog(self._window, 0, Gtk.MessageType.INFO,
+                Gtk.ButtonsType.NONE, title)
+            dialog.format_secondary_text(message)
+            dialog.show()
 
     def on_unload(self):
-        if self.notifier is not None:
-            self.notifier.remove()
+        self._window = None
