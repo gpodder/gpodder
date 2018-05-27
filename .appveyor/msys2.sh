f4aab0c4f79d56e652c828fd82c5badbce9540b3
Eric Le Lay <elelay@macports.org>
2018-05-27 11:38:26 +0200

    print bash cmds and exit on error

--- .appveyor/msys2.sh
+++ .appveyor/msys2.sh
@@ -30,4 +30,4 @@ export MSYS2_FC_CACHE_SKIP=1
 #pip3 install feedparser musicbrainzngs mutagen pycodestyle pyflakes
 
 cd tools/win_installer
-MSYSTEM=MINGW32 ./build.sh
+MSYSTEM=MINGW32 bash -xe ./build.sh
