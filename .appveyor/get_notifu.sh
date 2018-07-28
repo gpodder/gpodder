ba3f4084b3993d470b2a6b43b924386e60252ae5
Eric Le Lay <elelay@macports.org>
2018-07-28 22:29:18 +0200

    win_installer: fetch and verify using bash

---
+++ .appveyor/get_notifu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+src=notifu-src-1.6.1.zip
+wget https://www.paralint.com/projects/notifu/dl/$src
+checksum=$(sha256sum $src | cut -d " " -f0)
+if [ "$checksum" -ne "0fdcd08d3e12d87af76cdaafbf1278c4fcf1baf5d6447cce1a676b8d78a4d8c3" ]; then
+	echo "$src checksum OK"
+else
+	echo "$src checksum KO: got $checksum"
+	exit 1
+fi
