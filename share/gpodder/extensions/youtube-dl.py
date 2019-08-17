514e25432410aee653b34c1ab45f35fa7573b0a1
Eric Le Lay <elelay@macports.org>
2019-08-17 17:58:01 +0200

    isort import order

--- share/gpodder/extensions/youtube-dl.py
+++ share/gpodder/extensions/youtube-dl.py
@@ -10,13 +10,11 @@ import os
 import re
 import time
 
-import youtube_dl
-from youtube_dl.utils import sanitize_url
-
 import gpodder
+import youtube_dl
 from gpodder import download, feedcore, model, registry, youtube
 from gpodder.util import mimetype_from_extension, remove_html_tags
-
+from youtube_dl.utils import sanitize_url
 
 _ = gpodder.gettext
 
