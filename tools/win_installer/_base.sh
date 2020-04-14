43de4c6eb61f8307e4352fab9c31e21fe80cefa9
Eric Le Lay <elelay@macports.org>
2020-04-14 16:42:37 +0200

    update certifi

--- tools/win_installer/_base.sh
+++ tools/win_installer/_base.sh
@@ -90,7 +90,7 @@ git+https://github.com/enthought/pywin32-ctypes.git@f27d6a0
 html5lib==1.0.1
 webencodings==0.5.1
 six==1.12.0
-certifi==2019.9.11
+certifi==2020.4.5.1
 mutagen==1.44.0
 youtube_dl==2020.3.8
 "
