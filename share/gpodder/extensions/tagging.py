fa5c217c9ee76cec61d9b4982a8532c6bc9b2690
Eric Le Lay <elelay@macports.org>
2020-04-15 09:44:18 +0200

    fix error when tagging m4a files

--- share/gpodder/extensions/tagging.py
+++ share/gpodder/extensions/tagging.py
@@ -31,6 +31,7 @@ import os
 
 from mutagen import File
 from mutagen.easyid3 import EasyID3
+from mutagen.easymp4 import EasyMP4Tags
 from mutagen.flac import Picture
 from mutagen.id3 import APIC, ID3
 from mutagen.mp3 import MP3, EasyMP3
@@ -195,6 +196,8 @@ class gPodderExtension:
         # fix #737 EasyID3 doesn't recognize subtitle and comment tags
         EasyID3.RegisterTextKey("comments", "COMM")
         EasyID3.RegisterTextKey("subtitle", "TIT3")
+        EasyMP4Tags.RegisterTextKey("comments", "desc")
+        EasyMP4Tags.RegisterFreeformKey("subtitle", "SUBTITLE")
 
     def on_episode_downloaded(self, episode):
         info = self.read_episode_info(episode)
