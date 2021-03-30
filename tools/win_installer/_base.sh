bcb8d592a81e73f46288d1ded288a8a7cc590468
auouymous <au@qzx.com>
2021-03-30 17:54:20 -0600

    Change to jaraco's pywin32-ctypes repo.

--- tools/win_installer/_base.sh
+++ tools/win_installer/_base.sh
@@ -86,7 +86,7 @@ function extract_installer {
 PIP_REQUIREMENTS="\
 podcastparser==0.6.6
 mygpoclient==1.8
-git+https://github.com/enthought/pywin32-ctypes.git@f27d6a0
+git+https://github.com/jaraco/pywin32-ctypes.git@f27d6a0
 html5lib==1.1
 webencodings==0.5.1
 certifi==2020.11.8
