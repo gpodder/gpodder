12556a1a5e2ec3766f0e89034a60476f17fd07fb
Teemu Ikonen <tpikonen@gmail.com>
2021-06-10 12:19:41 +0300

    Add +1 to version in __init__.py

--- src/gpodder/__init__.py
+++ src/gpodder/__init__.py
@@ -20,13 +20,13 @@
 # This metadata block gets parsed by setup.py - use single quotes only
 __tagline__ = 'Media aggregator and podcast client'
 __author__ = 'Thomas Perl <thp@gpodder.org>'
-__version__ = '3.10.20'
+__version__ = '3.10.20+1'
 __date__ = '2021-06-06'
 __copyright__ = '© 2005-2021 The gPodder Team'
 __license__ = 'GNU General Public License, version 3 or later'
 __url__ = 'http://gpodder.org/'
 
-__version_info__ = tuple(int(x) for x in __version__.split('.'))
+#__version_info__ = tuple(int(x) for x in __version__.split('.'))
 
 import gettext
 import locale
