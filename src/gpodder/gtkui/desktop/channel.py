fe161d0391e4b531da31c3381a0ad762952465c8
Teemu Ikonen <tpikonen@gmail.com>
2021-06-23 13:20:43 +0300

    Set transient parent in Python code
    This was set in the ui-file, but Glade does not like it.

--- src/gpodder/gtkui/desktop/channel.py
+++ src/gpodder/gtkui/desktop/channel.py
@@ -32,6 +32,7 @@ class gPodderChannel(BuilderWidget):
     def new(self):
         self.show_on_cover_load = True
 
+        self.gPodderChannel.set_transient_for(self.parent_widget)
         self.title_label.set_text(self.channel.title)
         self.labelURL.set_text(self.channel.url)
         self.skip_feed_update_switch.set_active(self.channel.pause_subscription)
