cac4998119f8e4fbbb6d69793f9e682c12f5491f
auouymous <au@qzx.com>
2023-10-11 17:10:11 -0600

    prepare 3.11.4 release

--- src/gpodder/__init__.py
+++ src/gpodder/__init__.py
@@ -20,8 +20,8 @@
 # This metadata block gets parsed by setup.py - use single quotes only
 __tagline__ = 'Media aggregator and podcast client'
 __author__ = 'Thomas Perl <thp@gpodder.org>'
-__version__ = '3.11.3'
-__date__ = '2023-09-30'
+__version__ = '3.11.4'
+__date__ = '2023-10-11'
 __copyright__ = '© 2005-2023 The gPodder Team'
 __license__ = 'GNU General Public License, version 3 or later'
 __url__ = 'http://gpodder.org/'
