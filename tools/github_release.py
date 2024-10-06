657001fd4bd15cbdf0cd24e7b17d090d8bb46231
auouymous <au@qzx.com>
2024-10-06 00:07:57 -0600

    Download Mac artifact from GitHub instead of CircleCI.

--- tools/github_release.py
+++ tools/github_release.py
@@ -49,14 +49,36 @@ def download_items(urls, prefix):
                     f.write(chunk)
 
 
-def download_circleci(circleci, prefix):
-    """ download build artifacts from circleCI and exit """
-    print("I: downloading release artifacts from Circle.ci")
-    artifacts = requests.get("https://circleci.com/api/v1.1/project/github/gpodder/gpodder/%s/artifacts" % circleci).json()
-    items = {u["url"] for u in artifacts if re.match(r".+/gPodder-.+\.zip$", u["path"])}
-    if len(items) == 0:
+def download_mac_github(github_workflow, prefix, version):
+    """ download mac workflow artifacts from github and exit """
+    headers = {'Accept': 'application/vnd.github+json', 'Authorization': 'token %s' % github_token}
+
+    print("I: downloading release artifacts for workflow %d" % github_workflow)
+    r = requests.get("https://api.github.com/repos/gpodder/gpodder/actions/artifacts", headers=headers)
+    if not r.ok:
+        error_exit('ERROR: API fetch failed %d %s' % (r.status_code, r.reason))
+    artifacts = r.json()
+    artifact = [(a['id'], a['archive_download_url']) for a in artifacts['artifacts'] if a['workflow_run']['id'] == github_workflow]
+    if len(artifact) != 1:
         error_exit("Nothing found to download")
-    download_items(items, prefix)
+    id, url = artifact[0]
+    print("I: found artifact %d" % id)
+
+    print("I: downloading %s" % url)
+    output = os.path.join("_build", "{}-artifact.zip".format(prefix))
+    with requests.get(url, stream=True, headers=headers) as r:
+        if not r.ok:
+            error_exit('ERROR: artifact fetch failed %d %s' % (r.status_code, r.reason))
+        with open(output, "wb") as f:
+            for chunk in r.iter_content(chunk_size=1000000):
+                f.write(chunk)
+    print("I: unzipping %s" % output)
+    with zipfile.ZipFile(output, 'r') as z:
+        z.extractall('_build')
+    os.remove(output)
+    os.remove(os.path.join("_build", "{}-gPodder-{}.zip.md5".format(prefix, version)))
+    os.remove(os.path.join("_build", "{}-gPodder-{}.zip.sha256".format(prefix, version)))
+    checksum()
 
 
 def download_appveyor(appveyor_build, prefix):
@@ -106,7 +128,7 @@ def get_previous_tag():
     return latest_release.tag_name
 
 
-def release_text(tag, previous_tag, circleci=None, appveyor=None):
+def release_text(tag, previous_tag, mac_github=None, appveyor=None):
     t = Template("""
 Linux, macOS and Windows are supported.
 
@@ -128,7 +150,7 @@ Thanks to {{contributors[0]}}{% for c in contributors[1:-1] %}, {{c}}{% endfor %
  - ...
 
 ## CI references
- - macOS CircleCI build [{{circleci}}](https://circleci.com/gh/gpodder/gpodder/{{circleci}})
+ - macOS GitHub build [{{mac_github}}](https://github.com/gpodder/gpodder/actions/runs/{{mac_github}})
  - Windows Appveyor build [{{appveyor}}](https://ci.appveyor.com/project/elelay/gpodder/build/{{appveyor}})
 
 ## Checksums
@@ -140,14 +162,14 @@ Thanks to {{contributors[0]}}{% for c in contributors[1:-1] %}, {{c}}{% endfor %
             'contributors': get_contributors(tag, previous_tag),
             'tag': tag,
             'previous_tag': previous_tag,
-            'circleci': circleci,
+            'mac_github': mac_github,
             'appveyor': appveyor,
             'checksums': checksums()
     }
     return t.render(args)
 
 
-def upload(repo, tag, previous_tag, circleci, appveyor):
+def upload(repo, tag, previous_tag, mac_github, appveyor):
     """ create github release (draft) and upload assets """
     print("I: creating release %s" % tag)
     items = os.listdir('_build')
@@ -158,7 +180,7 @@ def upload(repo, tag, previous_tag, circleci, appveyor):
     except Exception as e:
         error_exit("Error creating release '%s' (%r)" % (tag, e))
     print("I: updating release description from template")
-    text = release_text(tag, previous_tag, circleci=circleci, appveyor=appveyor)
+    text = release_text(tag, previous_tag, mac_github=mac_github, appveyor=appveyor)
     print(text)
     if release.edit(body=text):
         print("I: updated release description")
@@ -181,17 +203,14 @@ def upload(repo, tag, previous_tag, circleci, appveyor):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='upload gpodder-osx-bundle artifacts to a github release\n'
         'Example usage: \n'
-        '    GITHUB_TOKEN=xxx python github_release.py --download --circleci 171 --appveyor 1.0.104 3.10.4\n'
-        '    GITHUB_TOKEN=xxx python github_release.py --circleci 171 --appveyor 1.0.104 3.10.4\n',
+        '    GITHUB_TOKEN=xxx python github_release.py --download --mac-github-workflow 1234567890 --appveyor 1.0.104 3.10.4\n'
+        '    GITHUB_TOKEN=xxx python github_release.py --mac-github-workflow 1234567890 --appveyor 1.0.104 3.10.4\n',
         formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('tag', type=str,
-                        help='gPodder git tag to create a release from')
-    parser.add_argument('--download', action='store_true',
-                        help='download artifacts from given circle.ci and appveyor build numbers')
-    parser.add_argument('--circleci', type=str, help='circleCI build number')
+    parser.add_argument('tag', type=str, help='gPodder git tag to create a release from')
+    parser.add_argument('--download', action='store_true', help='download artifacts')
+    parser.add_argument('--mac-github-workflow', type=str, help='mac github workflow number')
     parser.add_argument('--appveyor', type=str, help='appveyor build number')
-    parser.add_argument('--debug', '-d', action='store_true',
-                        help='debug requests')
+    parser.add_argument('--debug', '-d', action='store_true', help='debug requests')
 
     args = parser.parse_args()
 
@@ -206,14 +225,14 @@ if __name__ == "__main__":
     repo = gh.repository('gpodder', 'gpodder')
 
     if args.download:
-        if not args.circleci:
-            error_exit("E: --download requires --circleci number")
+        if not args.mac_github:
+            error_exit("E: --download requires --mac-github-workflow number")
         elif not args.appveyor:
             error_exit("E: --download requires --appveyor number")
         if os.path.isdir("_build"):
             error_exit("E: _build directory exists", -1)
         os.mkdir("_build")
-        download_circleci(args.circleci, "macOS")
+        download_mac_github(args.mac_github, "macOS", args.tag)
         download_appveyor(args.appveyor, "windows")
         print("I: download success.")
     else:
@@ -221,4 +240,4 @@ if __name__ == "__main__":
             error_exit("E: _build directory doesn't exist. You need to download build artifacts (see Usage)", -1)
 
     previous_tag = get_previous_tag()
-    upload(repo, args.tag, previous_tag, args.circleci, args.appveyor)
+    upload(repo, args.tag, previous_tag, args.mac_github, args.appveyor)
