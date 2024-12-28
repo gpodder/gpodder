fcf1b074b2540bdf64220198b245d7fb337ac0ac
Teemu Ikonen <tpikonen@gmail.com>
2024-12-28 17:52:01 +0200

    Prepare adaptive release 3.11.5+1

--- src/gpodder/__init__.py
+++ src/gpodder/__init__.py
@@ -21,7 +21,7 @@
 __tagline__ = 'Media aggregator and podcast client'
 __author__ = 'Thomas Perl <thp@gpodder.org>'
 __version__ = '3.11.5+1'
-__date__ = '2024-12-25'
+__date__ = '2024-12-28'
 __copyright__ = '© 2005-2024 The gPodder Team'
 __license__ = 'GNU General Public License, version 3 or later'
 __url__ = 'http://gpodder.org/'
