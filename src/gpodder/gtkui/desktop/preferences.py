a33fa42a48303dacccea8252aef3cbccd24c7689
Dana Conrad <dconrad@fastmail.com>
2025-01-28 22:34:28 -0600

    Device Preferences: add checkbox for "use absolute paths"
    Not sure if this setting applies to ipods

--- src/gpodder/gtkui/desktop/preferences.py
+++ src/gpodder/gtkui/desktop/preferences.py
@@ -384,6 +384,9 @@ class gPodderPreferences(BuilderWidget):
                                               self.checkbutton_compare_episode_filesize)
         self._config.connect_gtk_togglebutton('device_sync.one_folder_per_podcast',
                                               self.checkbutton_one_folder_per_podcast)
+        self._config.connect_gtk_togglebutton('device_sync.playlists.use_absolute_path',
+                                              self.checkbutton_playlists_use_absolute_path)
+        
 
         self.entry_custom_sync_name.set_text(self._config.device_sync.custom_sync_name)
 
@@ -792,6 +795,7 @@ class gPodderPreferences(BuilderWidget):
             self.combobox_episode_filename.set_sensitive(False)
             self.entry_custom_sync_name.set_sensitive(False)
             self.checkbutton_one_folder_per_podcast.set_sensitive(False)
+            self.checkbutton_playlists_use_absolute_path.set_sensitive(False)
         elif device_type == 'filesystem':
             self.btn_filesystemMountpoint.set_label(self._config.device_sync.device_folder or "")
             self.btn_filesystemMountpoint.set_sensitive(True)
@@ -804,6 +808,7 @@ class gPodderPreferences(BuilderWidget):
             self.entry_custom_sync_name.set_sensitive(
                 self._config.device_sync.custom_sync_name_enabled)
             self.checkbutton_one_folder_per_podcast.set_sensitive(True)
+            self.checkbutton_playlists_use_absolute_path.set_sensitive(True)
         elif device_type == 'ipod':
             self.btn_filesystemMountpoint.set_label(self._config.device_sync.device_folder)
             self.btn_filesystemMountpoint.set_sensitive(True)
@@ -816,6 +821,7 @@ class gPodderPreferences(BuilderWidget):
             self.combobox_episode_filename.set_sensitive(False)
             self.entry_custom_sync_name.set_sensitive(False)
             self.checkbutton_one_folder_per_podcast.set_sensitive(False)
+            self.checkbutton_playlists_use_absolute_path.set_sensitive(False)
         self.checkbutton_compare_episode_filesize.set_sensitive(True)
 
         children = self.btn_filesystemMountpoint.get_children()
