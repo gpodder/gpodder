030c5684bd1d88a24fffd8ba68db7a97aec6a86f
dconrad5 <dconrad@fastmail.com>
2025-03-02 08:10:22 -0600

    Update share/gpodder/extensions/tagging.py
    Print stacktrace if we can't open the channel image
    
    Co-authored-by: Eric Le Lay <222469+elelay@users.noreply.github.com>

--- share/gpodder/extensions/tagging.py
+++ share/gpodder/extensions/tagging.py
@@ -284,7 +284,7 @@ class gPodderExtension:
             with open(channel_image_filename, 'rb') as f:
                 channel_image = f.read()
         except:
-            logger.error("problems reading channel image!")
+            logger.exception("problems reading channel image!")
             channel_image = None
 
         embed_img = None
