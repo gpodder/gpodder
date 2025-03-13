f00bd46a6d4542cbb418de613c230a9d892c486f
Dana Conrad <dconrad@fastmail.com>
2025-03-12 21:33:42 -0500

    syncui: strip extra "/" from beginning of absolute paths when checking file
    With the creation of real absolute paths on devices, we need to strip
    the root "/" from the beginning of the path in order to check
    if the file still exists. In other words, change from the device POV
    to the computer POV.

--- src/gpodder/syncui.py
+++ src/gpodder/syncui.py
@@ -22,6 +22,7 @@
 # Ported to gPodder 3 by Joseph Wickremasinghe in June 2012
 
 import logging
+import re
 
 import gpodder
 from gpodder import sync, util
@@ -256,10 +257,11 @@ class gPodderSyncUI(object):
                                 # if playlist doesn't exist (yet) episodes_in_playlist will be empty
                                 if episodes_in_playlists:
                                     for episode_filename in episodes_in_playlists:
+                                        abs_path_episode_noslash = re.sub(r'^/', '', episode_filename)
                                         if ((not self._config.device_sync.playlists.use_absolute_path
                                         and not playlist.playlist_folder.resolve_relative_path(episode_filename).query_exists())
                                         or (self._config.device_sync.playlists.use_absolute_path
-                                        and not playlist.mountpoint.resolve_relative_path(episode_filename).query_exists())):
+                                        and not playlist.mountpoint.resolve_relative_path(abs_path_episode_noslash).query_exists())):
                                             # episode was synced but no longer on device
                                             # i.e. must have been deleted by user, so delete from gpodder
                                             try:
