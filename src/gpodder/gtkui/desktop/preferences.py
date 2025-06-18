039a8ae74b6b867a44b4be94618c599e33d68f6a
Teemu Ikonen <tpikonen@gmail.com>
2025-06-18 15:44:19 +0300

    preferences: Fix typo on user_extension_directory

--- src/gpodder/gtkui/desktop/preferences.py
+++ src/gpodder/gtkui/desktop/preferences.py
@@ -636,7 +636,7 @@ class gPodderPreferences(BuilderWidget):
         util.gui_open(gpodder.user_extensions.builtins_directory, gui=self)
 
     def on_button_user_extensions_clicked(self, widget):
-        util.gui_open(gpodder.user_extensions.user_extentions_directory, gui=self)
+        util.gui_open(gpodder.user_extensions.user_extension_directory, gui=self)
 
     def on_combo_audio_player_app_changed(self, widget):
         index = self.combo_audio_player_app.get_active()
