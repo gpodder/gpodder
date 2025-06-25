e192be71cb04cdbec0228198f0902390e25bfec5
Teemu Ikonen <tpikonen@gmail.com>
2025-06-25 23:28:42 +0300

    download: Set DownloadTask.DONE icon to selection-mode-symbolic
    This icon is easier to distinguish from the go-down icon used for the
    DOWNLOADING state.

--- src/gpodder/gtkui/download.py
+++ src/gpodder/gtkui/download.py
@@ -65,7 +65,7 @@ class DownloadStatusModel(Gtk.ListStore):
         # Set up stock icon IDs for tasks
         self._status_ids = collections.defaultdict(lambda: None)
         self._status_ids[download.DownloadTask.DOWNLOADING] = 'go-down'
-        self._status_ids[download.DownloadTask.DONE] = 'object-select-symbolic'
+        self._status_ids[download.DownloadTask.DONE] = 'selection-mode-symbolic'
         self._status_ids[download.DownloadTask.FAILED] = 'dialog-error'
         self._status_ids[download.DownloadTask.CANCELLING] = 'media-playback-stop'
         self._status_ids[download.DownloadTask.CANCELLED] = 'media-playback-stop'
