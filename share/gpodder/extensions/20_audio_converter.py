b42ad0dbc486828039f7fe005c298b4709a22fa8
Zachary Spector <public@zacharyspector.com>
2026-05-06 15:43:39 +1200

    Only show the ffmpeg error message in the notification
    Apparently the whole rest of the output is also sent to stderr...

--- share/gpodder/extensions/20_audio_converter.py
+++ share/gpodder/extensions/20_audio_converter.py
@@ -142,7 +142,7 @@ class gPodderExtension:
             gpodder.user_extensions.on_notification_show(_('File converted'), episode.title)
         else:
             logger.warning('Error converting audio file: %s / %s', stdout, stderr)
-            gpodder.user_extensions.on_notification_show(_('Conversion failed'), "%s: %s" % (episode.title, stderr))
+            gpodder.user_extensions.on_notification_show(_('Conversion failed'), "%s: %s" % (episode.title, stderr[stderr.rfind("Error"):])
         del self.futures[old_filename]
 
     def _convert_episode(self, episode):
